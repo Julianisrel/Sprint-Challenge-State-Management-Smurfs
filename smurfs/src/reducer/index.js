2a6139039df4462c81bcf1f3ac1c2fddc695225b
Julianisrel <52305213+Julianisrel@users.noreply.github.com>
2020-06-28 17:26:22 -0700

    working on forms

--- smurfs/src/reducer/index.js
+++ smurfs/src/reducer/index.js
@@ -6,3 +6,47 @@ const initialState = {
     error: ''
 };
 
+const reducer = (state = initialState, action) => {
+    console.log('action', action)
+    switch (action.type) {
+      case FETCH_SMURFS_START:
+        return {
+          ...state,
+          isFetching: true,
+          error: ""
+        };
+        case FETCH_SMURFS_SUCCESS:
+        return {
+          ...state,
+          smurfs: action.payload,
+          isFetching: false,
+          error: ""
+        };
+
+        case FETCH_SMURFS_FAILURE:
+          return {
+              ...state,
+              isFetching: false,
+              error: action.payload
+          }
+      case ADDING_SMURF:
+        return {
+          ...state,
+          isFetching: true,
+          error: ""
+        };
+
+        case ADD_SMURF:
+        return {
+          ...state,
+          isFetching: false,
+          error: "",
+          smurfs: action.payload
+        };
+
+      default:
+        return state;
+    }
+}
+
+export default reducer;
